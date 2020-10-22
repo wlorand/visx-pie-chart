05abc84b2be04807de677907ca32e8b4e711fecc
Will Lorand <will@milemarker3.org>
2020-10-22 16:02:45 -0700

    implemented first cut of Pie Chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+// child components
+import BrowserSharePieChart from './components/BrowserSharePieChart';
+
+// styles
 import './app.css';
 
 function App() {
   return (
     <div className="app">
       <h1 className="app-title">VisX Pie Chart Proto</h1>
+      <BrowserSharePieChart width={640} height={480} />
     </div>
   );
 }
---
+++ src/components/BrowserSharePieChart.tsx
@@ -0,0 +1,96 @@
+// new way to import React -- no default export, so TS complains on import React from 'react'
+import * as React from 'react';
+
+// @visx imports
+import { scaleOrdinal } from '@visx/scale';
+import Pie, { ProvidedProps, PieArcDatum } from '@visx/shape/lib/shapes/Pie';
+
+import { Group } from '@visx/group';
+import { GradientPinkBlue } from '@visx/gradient';
+
+// mock data
+import browserUsage, {
+  BrowserUsage as Browsers,
+} from '@visx/mock-data/lib/mocks/browserUsage';
+
+console.log(browserUsage); // big Array of browserShare data
+
+// define TS types for your data
+type BrowserNames = keyof Browsers;
+interface BrowserUsage {
+  label: BrowserNames;
+  usage: number;
+}
+
+// define vars for the browser data
+const browserNames = Object.keys(browserUsage[0]).filter(
+  (key) => key !== 'date'
+) as BrowserNames[];
+
+// **create browser object whose data is now ready to render as a pie (usage numbers add up to 100%)
+const browsers: BrowserUsage[] = browserNames.map((name) => ({
+  label: name,
+  usage: Number(browserUsage[0][name]),
+}));
+console.table(browsers);
+
+// data accessor function
+const usage = (d: BrowserUsage) => d.usage;
+
+// define color scale for your data
+const getBrowserColor = scaleOrdinal({
+  domain: browserNames,
+  range: [
+    'rgba(255,255,255,0.7)',
+    'rgba(255,255,255,0.6)',
+    'rgba(255,255,255,0.5)',
+    'rgba(255,255,255,0.4)',
+    'rgba(255,255,255,0.3)',
+    'rgba(255,255,255,0.2)',
+    'rgba(255,255,255,0.1)',
+  ],
+});
+
+// d3 margin convention
+const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+
+// define TS type for your props
+export type PieProps = {
+  width: number;
+  height: number;
+};
+
+function BrowserSharePieChart({ width, height }: PieProps) {
+  if (width < 100) return null; // don't render at widths less than 100 pixels
+
+  // define chart dimension vars
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+  // size the pie
+  const radius = Math.min(innerWidth, innerHeight) / 2;
+  // center the pie
+  const centerY = innerHeight / 2;
+  const centerX = innerWidth / 2;
+  // size the donut hole
+  const donutThickness = 75;
+
+  return (
+    <svg width={width} height={height}>
+      <GradientPinkBlue id="pie-bground" />
+      <rect rx={25} width={width} height={height} fill="url('#pie-bground')" />
+      <Group top={centerY + margin.top} left={centerX + margin.left}>
+        <Pie
+          data={browsers}
+          pieValue={usage}
+          outerRadius={radius}
+          innerRadius={radius - donutThickness}
+          cornerRadius={3}
+          padAngle={0.005}
+          fill={'rgba(255, 255, 255, 0.5)'} // need accessor
+        />
+      </Group>
+    </svg>
+  );
+}
+
+export default BrowserSharePieChart;
